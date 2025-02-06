5e97d52edcd9380b9f218d2a9e77aa5071b918c3
SUUDHI <sudhanshumeena123@gmail.com>
2025-02-06 11:36:11 +0530

    assigment 1 done

---
+++ day1/assigment/assigment-1.py
@@ -0,0 +1,9 @@
+#this assigment one take input from user as a string and cinvert them into a correct formate and display it 
+
+Name = input("enter your name:")
+Age = int(input("enter your age:"))#iam taking age as an int input,it aslp can done with takind a extra varible like age2=int(age)
+Hobby= input("Enter your favorite hobby:")
+
+print("name",Name,"Type:",type(Name))
+print("Age",Age,"Type:",type(Age))
+print("Hobby",Hobby,"Type:",type(Hobby))
