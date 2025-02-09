3cf8c50b8829728f2e3f20965b0d66dfebfdc3f3
SUUDHI <sudhanshumeena123@gmail.com>
2025-02-09 18:33:16 +0530

    add tsak 3

---
+++ day2/task_3.py
@@ -0,0 +1,36 @@
+"""
+Task 3: Password Strength Checker
+Write a program that:
+Takes a password input from the user.
+Checks and prints whether the password is strong or weak based on:
+At least 8 characters long
+Contains both uppercase and lowercase letters
+Includes at least one number
+Example Output:
+Enter a password: Hello123  
+Your password is strong!  
+"""
+passwd = input("enter a password: ")
+
+passwd_len = len(passwd) >= 8
+low_check = False
+up_check = False
+dig_check = False
+
+for char in passwd:
+    if 'A'<= char <= 'z':
+        up_check = True
+
+    if 'a'<= char <= 'z':
+        low_check = True
+
+    if '0' <= char <= '9':
+        dig_check = True
+
+if passwd_len and low_check and up_check and dig_check:
+    print("strong password")
+
+else:
+    print("week password")
+
+
