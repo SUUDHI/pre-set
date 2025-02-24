1f2d00ffcf8d58a0a717945591d558f6fb982a44
SUUDHI <sudhanshumeena123@gmail.com>
2025-02-24 18:14:32 +0530

    add set 1

---
+++ Question set-1/01 Basic List Operations.py
@@ -0,0 +1,10 @@
+"""
+Basic List Operations
+Create a list containing the numbers 1 to 5.
+Append the number 6 to the list.
+Remove the number 3 from the list.
+Print the final list."""
+l1=[1,2,3,4,5]
+l1.append(6)
+l1.remove(3)
+print(l1)
