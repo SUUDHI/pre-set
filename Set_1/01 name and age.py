0516e1a463459e795083de3fb4f0ef6089ab1723
SUUDHI <sudhanshumeena123@gmail.com>
2025-02-26 15:21:26 +0530

    remove question

--- Set_1/01 name and age.py
+++ Set_1/01 name and age.py
@@ -1,24 +1,3 @@
-""" 
-Create a dictionary to store the names and ages of three people. Write a program to:
-Print the age of a specific person from the dictionary.
-Add a new person with their age to the dictionary.
-Update the age of an existing person.
-Remove one person from the dictionary.
-"""
-# d={ 'name_1':'sudhanshu , 21',
-#     'name_2':'ram , 10',
-#     'name_3':'sham , 21'
-#     }
-# print(d['name_2'])
-# d ['name_4']='lala , 50'
-
-# d['name_2']= 'ram , 22'
-
-# del d['name_3']
-# print(d)
-
-# print()
-
 dic={'ram'    :22,
      'sham'   :20,
      'sharma' :30}
--- Set_1/02 set add&remove.py
+++ Set_1/02 set add&remove.py
@@ -1,8 +1,3 @@
-"""Create a set containing five unique numbers. Perform the following operations:
-Add a new number to the set.
-Try adding a duplicate number and observe what happens.
-Remove a specific number from the set.
-Check whether a given number is present in the set or not."""
 set1={1,2,3,4,5}
 set1.add(6)
 print(set1)
