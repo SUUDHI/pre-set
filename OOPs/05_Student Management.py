185f60d391ecf6d8505a3fe2fb3f350fe2063e0a
SUUDHI <sudhanshumeena123@gmail.com>
2025-03-05 16:55:15 +0530

    add

--- OOPs/05_Student Management.py
+++ OOPs/05_Student Management.py
@@ -4,6 +4,7 @@ class student:
         self.marks=marks
         self.name=name
         self.roll_no=roll_no
+        self._protected = "Protected"
     
     def avg(self):
         print(sum(self.marks.values()) / len(self.marks))
@@ -12,6 +13,7 @@ s1=student("sudhanshu",11,{"Math": 50, "Science": 60})
 s2=student("me",12,{"Math": 100, "Science": 60})
 s1.avg()
 s2.avg()
+print(s1._protected)
 
 
 
