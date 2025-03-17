9c6f11157e0848337631c6011696dc00f3ff3ce1
SUUDHI <sudhanshumeena123@gmail.com>
2025-03-17 10:35:02 +0530

    create view.py

---
+++ Employee Management System/view.py
@@ -0,0 +1,17 @@
+class EmployeeView:
+    @staticmethod
+    def show_message(message):
+        print(message)
+    
+    @staticmethod
+    def get_input(prompt):
+        return input(prompt)
+    
+    @staticmethod
+    def show_employee_details(employee):
+        print("Employee details")
+        print(f"ID:{employee['emp_id']}")
+        print(f"Name:{employee['name']}")
+        print(f"Age:{employee['age']}")
+        print(f"Department:{employee['department']}")
+        print(f"Salary:{employee['salary']}")
