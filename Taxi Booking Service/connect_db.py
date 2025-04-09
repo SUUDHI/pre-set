c07a781a4c9ce0caf92550ff9ea907ddc714b3c7
SUUDHI <sudhanshumeena123@gmail.com>
2025-04-09 11:33:02 +0530

    Untrack Taxi Booking Service

--- Taxi Booking Service/connect_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-DATABASE_NAME = "taxi_booking.db"
-
-def get_db_connection():
-    conn = sqlite3.connect("taxi_booking.db", check_same_thread=False)
-    conn.row_factory = sqlite3.Row  
-    return conn
